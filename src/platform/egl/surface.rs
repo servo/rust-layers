5b3363f56b7900c6b94e5afce0239f960814c518
ngohara <ngohara@ncsu.edu>
2015-12-02 22:37:10 -0500

    fixed import BGRA

--- src/platform/egl/surface.rs
+++ src/platform/egl/surface.rs
@@ -14,7 +14,7 @@ use texturegl::Texture;
 use egl::egl::{EGLDisplay, GetCurrentDisplay};
 use egl::eglext::{EGLImageKHR, DestroyImageKHR};
 use euclid::size::Size2D;
-use gleam::gl::{/*egl_image_target_texture2d_oes,*/ TEXTURE_2D, TexImage2D, BGRA_EXT, UNSIGNED_BYTE};
+use gleam::gl::{/*egl_image_target_texture2d_oes,*/ TEXTURE_2D, TexImage2D, BGRA, /*BGRA_EXT,*/ UNSIGNED_BYTE};
 use libc::c_void;
 use skia::gl_context::{GLContext, PlatformDisplayData};
 use skia::gl_rasterization_context::GLRasterizationContext;
@@ -69,11 +69,11 @@ impl EGLImageNativeSurface {
                     unsafe {
                         TexImage2D(TEXTURE_2D,
                                    0,
-                                   BGRA_EXT as i32,
+                                   BGRA as i32,
                                    self.size.width as i32,
                                    self.size.height as i32,
                                    0,
-                                   BGRA_EXT as u32,
+                                   BGRA as u32,
                                    UNSIGNED_BYTE,
                                    data);
                      }
